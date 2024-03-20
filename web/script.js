d6e36bd69495b409506bee302b4988b1146f4556
wulufu <wolf_neal@hotmail.com>
2024-03-20 17:28:36 -0400

    added basic search box and brought some controls back

--- web/script.js
+++ web/script.js
@@ -41,14 +41,53 @@ function previewImage() {
 
 async function initMap() {
     const {Map} = await google.maps.importLibrary("maps");
+    const {SearchBox} = await google.maps.importLibrary("places")
+    const {ControlPosition} = await google.maps.importLibrary("core")
+    const {LatLngBounds} = await google.maps.importLibrary("core")
 
     map = new Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
+        center: {lat: 42.684, lng: -73.827},
+        zoom: 15,
         mapTypeId: "satellite",
         tilt: 0,
-        disableDefaultUI: true,
+        mapTypeControl: false,
+        streetViewControl: false,
         keyboardShortcuts: false
+    });
+
+    // Create the search box and link it to the UI element.
+    const input = document.getElementById("searchBox");
+    const searchBox = new SearchBox(input);
+  
+    map.controls[ControlPosition.TOP_LEFT].push(input);
+
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener("places_changed", () => {
+        const places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // For each place, get the icon, name and location.
+        const bounds = new LatLngBounds();
+
+        places.forEach((place) => {
+            if (!place.geometry || !place.geometry.location) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+    
+        map.fitBounds(bounds);
     });
 }
 
